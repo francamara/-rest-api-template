1d928b5e1e512781a72844992813d4410612e2be
francamaradev <francamaradev@gmail.com>
2022-03-08 23:11:44 -0300

    feat: more stuff in the config file

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,8 @@
+const pkg = require('../../package.json');
+
 module.exports = {
-	env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  aplication_name: pkg.name,
+
 }
